ce71f7e1f28ac371198abcde31066289b85029b9
Amirdlz <54416476+aynorica@users.noreply.github.com>
2024-08-16 15:20:10 +0300

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,8 +26,8 @@ const config: Config = {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-				"main-bg": "url('/images/Bg.png')",
-				"profile-bg": "url('/images/profile-pic.png')",
+				"main-bg": "url('/portfolio//images/Bg.png')",
+				"profile-bg": "url('/portfolio//images/profile-pic.png')",
 			},
 			colors: {
 				primary: "#0A1128",
